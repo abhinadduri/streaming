3d33257fd23f912d661c126d5fea0c289cc8d831
Abhinav Adduri <abhinadduri@gmail.com>
2017-07-27 13:58:23 -0700

    working upload

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
-var host = "127.0.0.1";
-var port = 3000;
-var express = require("express");
+const host = "127.0.0.1";
+const port = 3000;
+const express = require('express');
+const busboy = require('connect-busboy');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 var app = express();
+require('express-ws')(app);
 app.use('/', express.static(__dirname + '/'));
+app.use(busboy());
+app.use(bodyParser.json());
 app.listen(port, host);
 
-console.log('Running server at http://localhost:' + port + '/');
+app.ws('/upload', function(ws, req) {
+  let index = 0;
+  let name = null;
+  ws.on('message', function(msg) {
+    if (index == 0) {
+      name = msg;
+    } else {
+      console.log(index);
+      fs.appendFileSync(name, msg);
+    }
+    index ++;
+  });
+
+  ws.on('close', function() {
+    console.log('closed')
+  })
+});
+
+app.post('/upload', (req, res) => {
+  req.pipe(req.busboy);
+  req.busboy.on('file', (fieldname, file, filename) => {
+    console.log(file)
+  });
+})
--- upload.js
+++ upload.js
@@ -1,1 +1,38 @@
-console.log('upload')
+function upload(event) {
+  const file = event.target.files[0];
+  const size = file.size;
+  const chunkSize = 64*1024*1024;
+  const socket = new WebSocket('ws://localhost:3000/upload');
+  let offset = 0;
+
+  const eventHandler = function(readEvent) {
+    if (readEvent.target.error == null) {
+      offset += chunkSize;
+      const result = readEvent.target.result;
+      socket.send(result);
+    } else {
+      console.error(readEvent.target.error);
+      return;
+    }
+    if (offset >= size) {
+      socket.close();
+      return;
+    }
+    readNextChunk(offset);
+  }
+
+  const readNextChunk = function(offset) {
+    const fr = new FileReader(file);
+    const blob = file.slice(offset, offset + chunkSize);
+    fr.onload = eventHandler;
+    fr.readAsArrayBuffer(blob);
+  }
+
+  socket.onopen = function() {
+    socket.send(file.name);
+    readNextChunk(offset);
+  }
+}
+
+const uploadButton = document.getElementById('uploadFile');
+uploadButton.addEventListener('change', upload);
